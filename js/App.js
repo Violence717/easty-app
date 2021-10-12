47276fdf4dab244b433c9b653ae587c8dd84c123
Violence717 <90672389+Violence717@users.noreply.github.com>
2021-10-12 10:50:36 +0600

    1.0.0

---
+++ js/App.js
@@ -0,0 +1,25 @@
+const menuCall = () => {
+    const hamburger = $('.hamburger');
+
+    hamburger.click(() => {
+
+        if ($('.hamburger__middle-item').hasClass('hamburger__middle-item_animated')) {
+            $('.hamburger__top-item').removeClass('hamburger__top-item_animated');
+            $('.hamburger__middle-item').removeClass('hamburger__middle-item_animated');
+            $('.hamburger__bottom-item').removeClass('hamburger__bottom-item_animated');
+
+            $('.circle').removeClass('pop-up-menu');
+            $('.search-form').addClass('search-form_hidden');
+        } else {
+            $('.hamburger__top-item').addClass('hamburger__top-item_animated');
+            $('.hamburger__middle-item').addClass('hamburger__middle-item_animated');
+            $('.hamburger__bottom-item').addClass('hamburger__bottom-item_animated');
+
+            $('.circle').addClass('pop-up-menu');
+            $('.search-form').removeClass('search-form_hidden');
+        }
+
+    })
+}
+
+menuCall();
